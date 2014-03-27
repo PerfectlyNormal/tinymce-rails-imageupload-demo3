f1b93d6ebf2ab9115f6bf30f783272ddece126c8
Per Christian B. Viken <perchr@northblue.org>
2014-03-27 18:30:07 +0100

    Empty Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinymceDummy3::Application.config.secret_token = '20f0a82a572c99bd0dee680faac1153f0e024d5afb1552097b747907a6c3d39e7060ade63b020bd1f43228ec262794b5bfc88499795a739d3a085ba4c30a3d65'
